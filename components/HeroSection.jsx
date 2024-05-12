32e8cd3d55d3d5489b688139849df930fa960bb2
Yogesh Patidar <yogeshpatidar340@gmail.com>
2024-05-12 13:29:41 +0530

    change

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -42,7 +42,7 @@ const HeroSection = () => {
               Hire Me
             </Link>
             <Link
-              href="/file/Yogesh Resume latest.pdf"
+              href="/file/yogesh full stack.pdf"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 to-pink-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
